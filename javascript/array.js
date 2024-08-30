05ae1366622a58f8d437efb570d47b9ab611a1f8
neerajn142004 <125648992+neerajn142004@users.noreply.github.com>
2024-08-30 10:06:38 +0530

    pl

---
+++ javascript/array.js
@@ -0,0 +1,13 @@
+var students =["Raj","Vinay","Sam","VIjay","Abdul"]
+console.log(students.length)
+
+//students.unshift("kurna")
+//console.log(students)
+console.log (students.slice(1,1))
+console.log(fewStudents1)
+console.log(students)
+var fewStudent = students.slice(1,3)
+console.log(fewStudents2)
+console.log(students)
+
+
---
+++ javascript/var.js
@@ -0,0 +1,8 @@
+var age =Date
+console.log(age)
+var sName ="Raj"
+if (age>18)    
+{
+     var sName = "Bunny"
+}
+console.log(sName)
